e47bb7c70c52d68393ce5380f17e1b3bf51b448d
Oskar <oskar@UX430UAR-2649a433.localdomain>
2023-04-25 15:47:21 +0200

    Implemented network save functionality

--- Pong/train.py
+++ Pong/train.py
@@ -6,6 +6,7 @@ import config
 from utils import preprocess
 from evaluate import evaluate_policy
 from dqn import DQN, ReplayMemory, optimize
+import pickle
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--env', choices=['ALE/Pong-v5'], default='ALE/Pong-v5')
 parser.add_argument('--evaluate_freq', type=int, default=50, help='How often to run evaluation.', nargs='?')
 parser.add_argument('--evaluation_episodes', type=int, default=5, help='Number of evaluation episodes.', nargs='?')
+parser.add_argument('--restart',type=int, default=0, help='Load networks from file in case training stops', nargs='?')
 
 # Hyperparameter configurations for different environments. See config.py.
 ENV_CONFIGS = {
@@ -21,7 +23,6 @@ ENV_CONFIGS = {
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
     # Initialize environment and config.
     env = gym.make(args.env)
     env_config = ENV_CONFIGS[args.env]
@@ -31,13 +32,21 @@ if __name__ == '__main__':
     # scale_obs = True, rescales observations from 0-255 to [0-1)
         env = AtariPreprocessing(env, screen_size = 84, grayscale_obs = True, frame_skip = 1, noop_max = 30, scale_obs = True)
 
-    # Initialize deep Q-networks.
-    dqn = DQN(env_config=env_config).to(device)
-    # Create and initialize target Q-network.
-    dqnTarget = DQN(env_config=env_config).to(device)
-
-    # Create replay memory.
-    memory = ReplayMemory(env_config['memory_size'])
+    
+    if ~args.restart:
+        # Initialize deep Q-networks.
+        dqn = DQN(env_config=env_config).to(device)
+        # Create and initialize target Q-network.
+        dqnTarget = DQN(env_config=env_config).to(device)
+        memory = ReplayMemory(env_config['memory_size'])
+    else:
+        # Load networks from file
+        with open("networks.obj", 'rb') as file:
+            network_list = pickle.load(file)
+            dqn = network_list[0]
+            dqnTarget = network_list[1]
+            memory = network_list[2]
+    
 
     # Initialize optimizer used for training the DQN. We use Adam rather than RMSProp.
     # dqn.parameter is from the base class nn.Module
@@ -91,11 +100,17 @@ if __name__ == '__main__':
             print(f'Episode {episode+1}/{env_config["n_episodes"]}: {mean_return}')
 
             # Save current agent if it has the best performance so far.
-            if mean_return >= best_mean_return:
+            if mean_return > best_mean_return:
                 best_mean_return = mean_return
 
                 #print('Best performance so far! Saving model.')
                 #torch.save(dqn, f'models/{args.env}_best.pt')
-        
+                # Save class instances
+                networks = [dqn, dqnTarget, memory]
+                print("Saving network to file..")
+                with open("networks.obj", 'wb') as file:
+                    pickle.dump(networks, file)
+                print("Save completed")
+
     # Close environment after training is completed.
     env.close()
